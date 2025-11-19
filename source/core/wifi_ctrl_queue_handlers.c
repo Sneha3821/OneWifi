7b7022c7aec498aab1ef0203f2134d0d1c06a52c
skanna952 <sneha_kannan@comcast.com>
2025-11-19 09:37:42 +0000

    Corrected the mistake

--- source/core/wifi_ctrl_queue_handlers.c
+++ source/core/wifi_ctrl_queue_handlers.c
@@ -3502,7 +3502,7 @@ void process_rsn_override_rfc(bool type)
         if (ret != RETURN_OK) {
             wifi_util_error_print(WIFI_CTRL,"%s:%d: Private vaps service update_fn failed \n",__func__, __LINE__);
         } else {
-            wwifi_util_error_print(WIFI_DB,"%s:%d:Sneha Updating security mode for apIndex %d secmode %d \n",__func__, __LINE__,apIndex,vapInfo->u.bss_info.security.mode);
+            wifi_util_error_print(WIFI_DB,"%s:%d:Sneha Updating security mode for apIndex %d secmode %d \n",__func__, __LINE__,apIndex,vapInfo->u.bss_info.security.mode);
         }
     }
 }
